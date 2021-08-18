2be1e0ac3aa79e44bb4419dbc1ff56b4818ad0ed
Yuta iwasaki <yanyong0127@gmail.com>
2021-08-18 16:29:00 +0900

    Initial Commit

---
+++ AkioSanTask7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AkioSanTask7
+//
+//  Created by IwasakIYuta on 2021/08/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
